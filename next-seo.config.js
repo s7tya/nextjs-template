da04f012132c68c34462a3f03b0529b0ef2b3add
s7tya <53410646+s7tya@users.noreply.github.com>
2021-06-28 11:45:50 +0900

    configure prettier

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,13 +1,13 @@
 export default {
   openGraph: {
-		site_name: "SiteName",
-		description: "Site description",
-		type: "website",
-    locale: "ja_JP",
-    url: "https://example.com/",
-    canonical: "https://example.com",
+    site_name: 'SiteName',
+    description: 'Site description',
+    type: 'website',
+    locale: 'ja_JP',
+    url: 'https://example.com/',
+    canonical: 'https://example.com',
   },
   twitter: {
-    cardType: "summary_large_image",
+    cardType: 'summary_large_image',
   },
-};
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,18 @@
-import SEO from "../../next-seo.config";
-import "../styles/globals.scss";
-import { DefaultSeo } from "next-seo";
-import Head from "next/head"
+import SEO from '../../next-seo.config'
+import '../styles/globals.scss'
+import { DefaultSeo } from 'next-seo'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>{ SEO.openGraph.site_name }</title>
+        <title>{SEO.openGraph.site_name}</title>
       </Head>
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
